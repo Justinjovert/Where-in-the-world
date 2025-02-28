362d41dd408111d8d856600f9a76859fcc8bf7a1
Justinjovert <justinjoags@gmail.com>
2025-02-28 14:41:55 +0800

    changed useState to useReducer

--- src/pages/Countries.jsx
+++ src/pages/Countries.jsx
@@ -1,10 +1,10 @@
-import { useMemo, useState } from 'react'
+import { useMemo, useReducer } from 'react'
 import '../styles/countries.css'
-import { NavLink, Outlet, useLoaderData, useParams } from 'react-router-dom'
+import { Outlet, useLoaderData, useParams } from 'react-router-dom'
 import CountryCard from '../Components/CountryCard'
 
 
-const regions = ['Africa', 'America', 'Asia', 'Europe', 'Oceania', 'World']
+const regions = ['Africa', 'Americas', 'Asia', 'Europe', 'Oceania', 'World']
 
 
 // Sort Countries
@@ -12,25 +12,30 @@ const sortCountries = (countries) => {
     return [...countries].sort((a, b) => a.name.common.localeCompare(b.name.common));
 };
 
+
+// UseReducer function
+function reduce(state, action) {
+    switch (action.type) {
+        case "SELECT_REGION":
+            return { ...state, selectedRegion: action.payload, dropDown: !state.dropDown }
+        case "TOGGLE_DROPDOWN":
+            return { ...state, dropDown: !state.dropDown }
+        case "SEARCH_QUERY":
+            return { ...state, searchQuery: action.payload }
+        default:
+            return state
+    }
+}
+
 // Main content that renders country cards with navigation ui
 function Countries() {
 
 
-    // States
-    const [searchQuery, setSearchQuery] = useState("")
-    const [dropDown, setDropDown] = useState(false)
-    const [selectedRegion, setSelectedRegion] = useState('World')
+    // Attempt to use useReducer
+    const [state, dispatch] = useReducer(reduce, { dropDown: false, selectedRegion: 'World', searchQuery: '' })
 
-    // Drop down for region select
-    const toggleDropdown = () => {
-        setDropDown((prev) => !prev)
-    }
 
-    // Select region and toggle dropdown
-    const handleSelectedRegion = (regionValue) => {
-        setSelectedRegion(regionValue)
-        setDropDown(false)
-    }
+    /* ---------------------- */
 
     // Parameter :country
     // When dynamic segment has value, render details of selected param
@@ -43,9 +48,9 @@ function Countries() {
     // Return searched countries using filter method
     const filteredCountries = useMemo(() => {
         return sortedCountries.filter(country =>
-            country.name.common.toLowerCase().includes(searchQuery.toLowerCase())
+            country.name.common.toLowerCase().includes(state.searchQuery.toLowerCase())
         )
-    }, [searchQuery, sortedCountries])
+    }, [state.searchQuery, sortedCountries])
 
     return (
         <>
@@ -63,11 +68,11 @@ function Countries() {
                                                 d="M464 428L339.92 303.9a160.48 160.48 0 0030.72-94.58C370.64 120.37 298.27 48 209.32 48S48 120.37 48 209.32s72.37 161.32 161.32 161.32a160.48 160.48 0 0094.58-30.72L428 464zM209.32 319.69a110.38 110.38 0 11110.37-110.37 110.5 110.5 0 01-110.37 110.37z" />
                                         </svg>
                                     </i>
-                                    <input value={searchQuery} className="searchbar" placeholder="Search for a country" onChange={(e) => setSearchQuery(e.target.value)} />
+                                    <input value={state.searchQuery} className="searchbar" placeholder="Search for a country" onChange={(e) => dispatch({ type: "SEARCH_QUERY", payload: e.target.value})} />
                                 </div>
                                 <div data-heading="front" className="region-select-wrapper">
-                                    <div className="region-selection-container" onClick={toggleDropdown}>
-                                        <span>{selectedRegion == 'World' ? 'Filter By Region' : selectedRegion}</span>
+                                    <div className="region-selection-container"  onClick={() => dispatch({ type: 'TOGGLE_DROPDOWN' })}>
+                                        <span>{state.selectedRegion == 'World' ? 'Filter By Region' : state.selectedRegion}</span>
                                         <i>
                                             <svg xmlns="http://www.w3.org/2000/svg" className="ionicon" viewBox="0 0 512 512" width="15px"
                                                 height="15px">
@@ -76,10 +81,10 @@ function Countries() {
                                             </svg>
                                         </i>
                                     </div>
-                                    {dropDown && (
+                                    {state.dropDown && (
                                         <div data-heading="front" className="region-options-container">
                                             {regions.map(region => (
-                                                <button key={region} value={region} onClick={() => handleSelectedRegion(region)}>{region}</button>
+                                                <button key={region} value={region} onClick={() => dispatch({ type: 'SELECT_REGION', payload: region })}>{region}</button>
                                             ))}
                                         </div>
                                     )}
@@ -87,15 +92,15 @@ function Countries() {
                             </section>
                             {/* // Display country cards */}
                             <section className='countries-container'>
-                                {selectedRegion == 'World'
+                                {state.selectedRegion == 'World'
                                     ?
                                     filteredCountries.map(country => (
-                                        <CountryCard key={country.cca3} {...country} searchQuery />
+                                        <CountryCard key={country.cca3} {...country} />
                                     ))
                                     :
                                     // Filter countries by region and use map to render countries
                                     filteredCountries
-                                        .filter(country => country.region.toUpperCase() === selectedRegion.toUpperCase())
+                                        .filter(country => country.region.toUpperCase() === state.selectedRegion.toUpperCase())
                                         .map(country => <CountryCard key={country.cca3} {...country} />)
                                 }
                             </section>
